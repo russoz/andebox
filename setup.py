69d9a3397172f14c9a4e54e8ca650c19baabd0a1
Alexei Znamensky <russoz@gmail.com>
2021-05-19 22:54:30 +1200

    Bump version: 0.12.1 → 0.12.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 requirements = [line.strip() for line in open('requirements.txt').readlines()]
 
 setup(name='andebox',
-      version='0.12.1',
+      version='0.12.2',
       scripts=['andebox'],
       install_requires=requirements,
       )
