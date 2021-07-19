a36a361027fe37b74e731d35d5ea1abfd1011089
Alexei Znamensky <russoz@gmail.com>
2021-07-19 23:06:27 +1200

    added andeboxlib package tree

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from setuptools import setup
+from setuptools import setup, find_packages
 
 requirements = [line.strip() for line in open('requirements.txt').readlines()]
 
 setup(name='andebox',
       version='0.13',
       scripts=['andebox'],
+      packages=find_packages(),
       install_requires=requirements,
       )
