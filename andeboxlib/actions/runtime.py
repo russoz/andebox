881c97663aedd9b69693a68c64a16d3bab345662
Alexei Znamensky <russoz@gmail.com>
2021-07-19 22:11:06 +1200

    added copyright notice to the top of files

--- andeboxlib/actions/runtime.py
+++ andeboxlib/actions/runtime.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+# (c) 2021, Alexei Znamensky <russoz@gmail.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 import argparse
 import os
